2cf1a0ae1d3a8c879e38356717b54b279cbcc3ae
JLaikows <joshua@laikowski.com>
2024-01-13 18:00:55 -0500

    Added Start Scripts

---
+++ server/README.md
@@ -0,0 +1,9 @@
+## To Run
+
+Open two seperate terminals. in the first, run
+
+`pnpm run build-watch`
+
+In the second, run
+
+`pnpm run dev`
--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,8 @@
+import Express from "express";
 
+const app = Express();
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`[Server]: Server is running at http://localhost:${port}`);
+});
