36eed8a7b022ede8ff76507937298e0de98df5db
JLaikows <joshua@laikowski.com>
2024-01-14 03:05:00 -0500

    Added Prettier

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,8 @@
-import Express from "express";
+import Express from 'express'
 
-const app = Express();
-const port = process.env.PORT;
+const app = Express()
+const port = process.env.PORT
 
 app.listen(port, () => {
-  console.log(`[Server]: Server is running at http://localhost:${port}`);
-});
+    console.log(`[Server]: Server is running at http://localhost:${port}`)
+})
