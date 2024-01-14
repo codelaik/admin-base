5d34adf60fa716a98e879789e7745af9aea4254c
JLaikows <joshua@laikowski.com>
2024-01-14 17:50:25 -0500

    Added default port

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,7 @@ import Express from 'express'
 import Logger from './src/lib/logger'
 
 const app = Express()
-const port = process.env.PORT
+const port = process.env.PORT || 8081
 
 app.listen(port, () => {
     Logger.info(`Server is running at http://localhost:${port}`)
