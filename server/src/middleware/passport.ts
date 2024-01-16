2a32abf60de76d355e0480a77e9e321f5cecb58c
JLaikows <joshua@laikowski.com>
2024-01-16 14:31:54 -0500

    Added passport middleware

---
+++ server/src/middleware/passport.ts
@@ -0,0 +1,27 @@
+import { Strategy, ExtractJwt, StrategyOptions } from 'passport-jwt'
+import db from '../lib/prisma'
+
+const jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+const secretOrKey = process.env.SECRET_OR_KEY
+const options: StrategyOptions = {
+    jwtFromRequest,
+    secretOrKey,
+}
+
+export const initializePassport = async (passport: any) => {
+    passport.use(
+        new Strategy(options, async (jwt_payload, done) => {
+            const user = await db.user.findUnique({
+                where: {
+                    id: jwt_payload.id,
+                },
+            })
+            if (user) {
+                // return the user to the frontend
+                return done(null, user)
+            }
+            // return false since there is no user
+            return done(null, false)
+        })
+    )
+}
