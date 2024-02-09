7fe9de236c0a1b1c00686375dcdc73eef40f9781
jplaikows <joshua@laikowski.com>
2024-02-09 13:59:52 -0500

    Updated description

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,6 +7,12 @@ export const exclude = (user: User, keys: keyof User) => {
     )
 }
 
+/**
+ * Encrypt a string for sensitive data (such as passwords). Be sure to await as the bycrypt function is asyncronous
+ * @async
+ * @param unencryptedPassword String
+ * @returns encryptedPassword as a string
+ */
 export const encrypt = async (unencryptedPassword: string) => {
     const salt = await bcrypt.genSalt(10)
     const encryptedPassword = await bcrypt.hash(unencryptedPassword, salt)
