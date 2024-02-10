cb745cd23991d9f25202f0714271de634b550121
jplaikows <joshua@laikowski.com>
2024-02-10 17:18:30 -0500

    Added Error Boundary

---
+++ ui/admin/src/pages/ErrorBoundary/index.tsx
@@ -0,0 +1,73 @@
+import { Box, Typography } from '@mui/material'
+import React, { Component } from 'react'
+import { COLORS } from '../../styles/theme'
+
+// export const ErrorBoundaryPage: FC<{ children: any }> = ({ children }) => {
+//     try {
+//         console.log('hit')
+//         return children
+//     } catch (e) {
+//         return (
+//             <Box
+//                 sx={{
+//                     display: 'flex',
+//                     justifyContent: 'center',
+//                     alignItems: 'center',
+//                     height: '100vh',
+//                     width: '100vw',
+//                     backgroundColor: COLORS.PRIMARY,
+//                 }}
+//             >
+//                 <Box>
+//                     <Typography>Something Broke :(</Typography>
+//                     <Typography>Refresh the page or try again later</Typography>
+//                 </Box>
+//             </Box>
+//         )
+//     }
+// }
+
+class ErrorBoundary extends Component<
+    { children: any },
+    { hasError: boolean }
+> {
+    constructor(props: { children: any }) {
+        super(props)
+        this.state = { hasError: false }
+    }
+
+    static getDerivedStateFromError(error: any) {
+        return { hasError: true }
+    }
+
+    componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+        console.log(`Error: ${error} | ${errorInfo}`)
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return (
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        height: '100vh',
+                        width: '100vw',
+                        backgroundColor: COLORS.PRIMARY,
+                    }}
+                >
+                    <Box>
+                        <Typography>Something Broke :(</Typography>
+                        <Typography>
+                            Refresh the page or try again later
+                        </Typography>
+                    </Box>
+                </Box>
+            )
+        }
+        return this.props.children
+    }
+}
+
+export default ErrorBoundary
