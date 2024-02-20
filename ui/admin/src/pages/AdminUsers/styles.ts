bfda5aeef2bd8b41e309ad5bd9a8aa24b6b84c6a
jplaikows <joshua@laikowski.com>
2024-02-20 14:55:55 -0500

    Added styling

--- ui/admin/src/pages/AdminUsers/styles.ts
+++ ui/admin/src/pages/AdminUsers/styles.ts
@@ -2,6 +2,10 @@ const styles = {
     pageContainer: {
         height: '100vh',
         padding: '20px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'left',
+        alignItems: 'left',
     }
 }
 
