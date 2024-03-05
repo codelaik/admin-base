8cdc8ba214b66a907e9ce30fabc82fe5ec0cefb3
jplaikows <joshua@laikowski.com>
2024-03-04 22:09:46 -0500

    Updated App for consumer

--- ui/consumer/src/App.tsx
+++ ui/consumer/src/App.tsx
@@ -1,22 +1,12 @@
-import React from 'react'
 import './App.css'
+import Navbar from './components/global/Navbar'
+import PhotoHeader from './components/global/PhotoHeader'
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className="text-3xl">
+            <Navbar />
+            <PhotoHeader />
         </div>
     )
 }
